46d318a42a748c098fb0aa8c7293453d3bf51feb
gustavo barros <gustavobarrosaraujo10@gmail.com>
2023-05-24 11:34:25 -0300

    App-Shop

---
+++ lib/utils/constants.dart
@@ -0,0 +1,6 @@
+class Constants {
+  static const productBaseUrl =
+      'https://app-shop-7ae22-default-rtdb.firebaseio.com/products';
+  static const orderBaseUrl =
+      'https://app-shop-7ae22-default-rtdb.firebaseio.com/orders';
+}
