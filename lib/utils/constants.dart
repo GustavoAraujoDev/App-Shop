c1c4b73679ca5dded6c8e94bd564783397b16979
gustavoaraujo11 <122275138+gustavoaraujo11@users.noreply.github.com>
2023-05-26 13:53:20 -0300

    Delete lib directory

--- lib/utils/constants.dart
+++
@@ -1,6 +0,0 @@
-class Constants {
-  static const productBaseUrl =
-      'https://app-shop-7ae22-default-rtdb.firebaseio.com/products';
-  static const orderBaseUrl =
-      'https://app-shop-7ae22-default-rtdb.firebaseio.com/orders';
-}
