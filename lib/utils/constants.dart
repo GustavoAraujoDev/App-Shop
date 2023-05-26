5ee94b63817a6479219a210dbcc9ac54a21ef11b
gustavoaraujo11 <122275138+gustavoaraujo11@users.noreply.github.com>
2023-05-26 14:07:05 -0300

    App-Shop

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,9 +1,9 @@
 class Constants {
   static const userFavoritesUrl =
-      'https://shop-cod3r-c70a4-default-rtdb.firebaseio.com/userFavorites';
+      'https://app-shop-7ae22-default-rtdb.firebaseio.com/userFavorites';
   static const productBaseUrl =
-      'https://shop-cod3r-c70a4-default-rtdb.firebaseio.com/products';
+      'https://app-shop-7ae22-default-rtdb.firebaseio.com/products';
   static const orderBaseUrl =
-      'https://shop-cod3r-c70a4-default-rtdb.firebaseio.com/orders';
-  static const webApiKey = 'AIzaSyAOB9uer_xxRprUZnoZSqeK3oenNmingOc';
+      'https://app-shop-7ae22-default-rtdb.firebaseio.com/orders';
+  static const webApiKey = 'AIzaSyBRGB2efDX2n-J9C975c0ZPZNW8BJSCmX8';
 }
